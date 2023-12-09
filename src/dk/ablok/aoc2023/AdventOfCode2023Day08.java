cd97a2d2674995d7d244e31a7a28c72eb92bd258
Anders Birk Sørensen <anders@ablok.dk>
2023-12-09 10:32:06 +0100

    Solve 2023-08 and 2023-09

--- src/dk/ablok/aoc2023/AdventOfCode2023Day08.java
+++ src/dk/ablok/aoc2023/AdventOfCode2023Day08.java
@@ -3,19 +3,144 @@ package dk.ablok.aoc2023;
 import dk.ablok.aoc.exceptions.AocLoadException;
 import dk.ablok.aoc.test.AocTestable;
 
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+import static dk.ablok.aoc.io.InputUtils.readInputAsListSeparateByEmptyLine;
+
 public class AdventOfCode2023Day08 implements AocTestable {
+    private byte[] directions;
+    private Set<Node> nodes;
+
     @Override
     public void load(String filename) throws AocLoadException {
+        List<List<String>> input = readInputAsListSeparateByEmptyLine(filename);
+        directions = input.get(0).get(0).getBytes();
 
+        nodes = input.get(1).stream()
+                .map(s -> s.substring(0, 3))
+                .map(Node::new)
+                .collect(Collectors.toSet());
+
+        for (String line : input.get(1)) {
+            Node here = nodes.stream().filter(n -> n.getName().equals(line.substring(0, 3))).findFirst().orElseThrow();
+            Node left = nodes.stream().filter(n -> n.getName().equals(line.substring(7, 10))).findFirst().orElseThrow();
+            Node right = nodes.stream().filter(n -> n.getName().equals(line.substring(12, 15))).findFirst().orElseThrow();
+
+            here.setLeft(left);
+            here.setRight(right);
+        }
     }
 
     @Override
     public String part1() {
-        return "";
+        Node here = nodes.stream().filter(n -> n.getName().equals("AAA")).findFirst().orElseThrow();
+
+        int i = 0;
+        while (!here.getName().equals("ZZZ")) {
+            here = here.getNext(directions[i % directions.length]);
+            i++;
+        }
+
+        return Integer.toString(i);
     }
 
     @Override
     public String part2() {
-        return "";
+        List<Node> start = nodes.stream().filter(Node::endsWithA).toList();
+        List<Long> cycles = new ArrayList<>();
+
+        for (int j = 0; j < start.size(); j++) {
+            Node here = start.get(j);
+
+            long i = 0;
+            while (!here.endsWithZ()) {
+                int finalI = (int) i;
+                here = here.getNext(directions[finalI % directions.length]);
+                i++;
+            }
+
+            cycles.add(i);
+        }
+
+        long product = 1;
+        for (long n : cycles) {
+            product *= n;
+        }
+
+        Map<Long, Long> powers = cycles.stream()
+                .flatMap(c -> getPrimeDivisors(c).entrySet().stream())
+                .collect(Collectors.toMap(Map.Entry<Long, Long>::getKey, Map.Entry<Long, Long>::getValue, (a, b) -> a.compareTo(b) >= 0 ? a : b));
+
+
+        return Long.toString(product);
+    }
+
+    private Map<Long, Long> getPrimeDivisors(long number) {
+        Map<Long, AtomicLong> primeDivisors = new HashMap<>();
+
+        while (number > 1) {
+            long i = 2;
+            while (number % i != 0 || !isPrime(i)) {
+                i++;
+            }
+            number = number / i;
+            primeDivisors.computeIfAbsent(i, x -> new AtomicLong(0));
+            primeDivisors.get(i).incrementAndGet();
+        }
+
+        return primeDivisors.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, x -> x.getValue().get()));
+    }
+
+    private boolean isPrime(long number) {
+        if (number <= 1) return false;
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    private static class Node {
+        private final String name;
+        private Node left;
+        private Node right;
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        public Node setLeft(Node left) {
+            this.left = left;
+            return this;
+        }
+
+        public Node setRight(Node right) {
+            this.right = right;
+            return this;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public boolean endsWithA() {
+            return name.charAt(2) == 'A';
+        }
+
+        public boolean endsWithZ() {
+            return name.charAt(2) == 'Z';
+        }
+
+        public Node getNext(byte direction) {
+            return direction == 'L' ? left : right;
+        }
+
+        @Override
+        public String toString() {
+            return name + " = (" + left.getName() + ", " + right.getName() + ")";
+        }
     }
 }
--- src/dk/ablok/aoc2023/AdventOfCode2023Day09.java
+++ src/dk/ablok/aoc2023/AdventOfCode2023Day09.java
@@ -3,19 +3,93 @@ package dk.ablok.aoc2023;
 import dk.ablok.aoc.exceptions.AocLoadException;
 import dk.ablok.aoc.test.AocTestable;
 
+import java.util.*;
+import java.util.stream.IntStream;
+
+import static dk.ablok.aoc.io.InputUtils.readInputAsList;
+
 public class AdventOfCode2023Day09 implements AocTestable {
+    private List<OasisSeries> input;
+
     @Override
     public void load(String filename) throws AocLoadException {
-
+        input = readInputAsList(filename).stream()
+                .map(line -> line.split(" "))
+                .map(a -> Arrays.stream(a).map(Long::parseLong).toList())
+                .map(OasisSeries::new)
+                .toList();
+        input.forEach(OasisSeries::calculateDiffs);
     }
 
     @Override
     public String part1() {
-        return "";
+        return Long.toString(
+                input.stream()
+                        .mapToLong(OasisSeries::extrapolateForwards)
+                        .sum());
     }
 
     @Override
     public String part2() {
-        return "";
+        return Long.toString(
+                input.stream()
+                        .mapToLong(OasisSeries::extrapolateBackwards)
+                        .sum());
+    }
+
+    private static class OasisSeries {
+        private final Map<Integer, List<Long>> values;
+
+        public OasisSeries(List<Long> values) {
+            this.values = new HashMap<>();
+            this.values.put(0, new ArrayList<>(values));
+        }
+
+        public void calculateDiffs() {
+            Map.Entry<Integer, List<Long>> lastEntry = values.entrySet().stream()
+                    .min(Map.Entry.comparingByKey())
+                    .orElseThrow();
+            int lastIndex = lastEntry.getKey();
+            List<Long> lastSeries = lastEntry.getValue();
+
+            while (!lastSeries.stream().allMatch(n -> n == 0)) {
+                lastIndex++;
+
+                List<Long> finalLastSeries = lastSeries;
+                lastSeries = IntStream.range(1, lastSeries.size())
+                        .mapToObj(i -> finalLastSeries.get(i) - finalLastSeries.get(i - 1))
+                        .toList();
+
+                values.put(lastIndex, new ArrayList<>(lastSeries));
+            }
+        }
+
+        private Long extrapolateForwards() {
+            // Extend last series
+            values.get(values.size() - 1).add(0L);
+
+            for (int i = values.size() - 1; i > 0; i--) {
+                List<Long> thisSeries = values.get(i - 1);
+                long lastInThisSeries = thisSeries.stream().reduce((first, second) -> second).orElseThrow();
+                long diff = values.get(i).stream().reduce((first, second) -> second).orElseThrow();
+                thisSeries.add(lastInThisSeries + diff);
+            }
+
+            return values.get(0).stream().reduce((first, second) -> second).orElseThrow();
+        }
+
+        private Long extrapolateBackwards() {
+            // Extend last series
+            values.get(values.size() - 1).add(0, 0L);
+
+            for (int i = values.size() - 1; i > 0; i--) {
+                List<Long> thisSeries = values.get(i - 1);
+                long firstInThisSeries = thisSeries.get(0);
+                long diff = values.get(i).get(0);
+                thisSeries.add(0,firstInThisSeries - diff);
+            }
+
+            return values.get(0).get(0);
+        }
     }
 }
