7995352f60c8f22367b92e9c89b263529b5bf642
Anders Birk Sørensen <anders@ablok.dk>
2025-12-06 18:38:44 +0100

    Solve 2025-05 and 2025-06

--- src/main/java/dk/ablok/aoc2025/AdventOfCode2025Day05.java
+++ src/main/java/dk/ablok/aoc2025/AdventOfCode2025Day05.java
@@ -1,10 +1,14 @@
 package dk.ablok.aoc2025;
 
-import dk.ablok.aoc.AocSolution;
 import dk.ablok.aoc.AocPuzzle;
+import dk.ablok.aoc.AocSolution;
 import dk.ablok.aoc.exceptions.AocLoadException;
 import dk.ablok.aoc.exceptions.AocSolveException;
 import dk.ablok.aoc.io.AocInput;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Solution to the Advent of Code 2025 day 5 puzzle
@@ -19,18 +23,68 @@ import dk.ablok.aoc.io.AocInput;
 @AocSolution(year = 2025, day = 5)
 public class AdventOfCode2025Day05 implements AocPuzzle {
 
+    private List<Interval> intervals;
+    private List<Long> ids;
+
     @Override
     public void load() throws AocLoadException {
         var input = new AocInput(2025, 5);
+        var sections = input.readInputAsListSeparateByEmptyLine();
+
+        intervals = sections.get(0).stream()
+                .map(s -> s.split("-"))
+                .map(a -> new Interval(Long.parseLong(a[0]), Long.parseLong(a[1])))
+                .sorted(Comparator.comparingLong(a -> a.start))
+                .toList();
+
+        ids = sections.get(1).stream()
+                .map(Long::parseLong)
+                .toList();
     }
 
     @Override
     public String part1() throws AocSolveException {
-        return null;
+        return Long.toString(ids.stream()
+                .filter(id -> intervals.stream().anyMatch(interval -> interval.contains(id)))
+                .count());
     }
 
     @Override
     public String part2() throws AocSolveException {
-        return null;
+        List<Interval> newIntervals = new ArrayList<>();
+
+        var first = intervals.get(0);
+        long start = first.start();
+        long end = first.end();
+
+        for (int i = 1; i < intervals.size(); i++) {
+            var interval = intervals.get(i);
+
+            if (interval.start() <= end) {
+                // Intervals overlap; merge them
+                if (interval.end() > end) {
+                    end = interval.end();
+                }
+            } else {
+                // No longer overlapping; store interval and start new
+                newIntervals.add(new Interval(start, end));
+                start = interval.start();
+                end = interval.end();
+            }
+        }
+
+        // Close last interval
+        newIntervals.add(new Interval(start, end));
+
+        return Long.toString(newIntervals.stream()
+                .mapToLong(i -> i.end() - i.start() + 1)
+                .sum());
+        // 436581420389585 Too high
+    }
+
+    record Interval(long start, long end) {
+        public boolean contains(long number) {
+            return start <= number && number <= end;
+        }
     }
 }
--- src/main/java/dk/ablok/aoc2025/AdventOfCode2025Day06.java
+++ src/main/java/dk/ablok/aoc2025/AdventOfCode2025Day06.java
@@ -1,10 +1,16 @@
 package dk.ablok.aoc2025;
 
-import dk.ablok.aoc.AocSolution;
 import dk.ablok.aoc.AocPuzzle;
+import dk.ablok.aoc.AocSolution;
 import dk.ablok.aoc.exceptions.AocLoadException;
 import dk.ablok.aoc.exceptions.AocSolveException;
 import dk.ablok.aoc.io.AocInput;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 /**
  * Solution to the Advent of Code 2025 day 6 puzzle
@@ -19,18 +25,97 @@ import dk.ablok.aoc.io.AocInput;
 @AocSolution(year = 2025, day = 6)
 public class AdventOfCode2025Day06 implements AocPuzzle {
 
+    private char[][] input;
+
     @Override
+
     public void load() throws AocLoadException {
-        var input = new AocInput(2025, 6);
+        var aocInput = new AocInput(2025, 6);
+        input = aocInput.read2dArray(' ');
     }
 
     @Override
     public String part1() throws AocSolveException {
-        return null;
+        Pattern pattern = Pattern.compile("[\\d+*]+");
+
+        // Parse operations and initialize problems
+        var operations = String.valueOf(input[input.length - 1]);
+        Matcher operationsMatcher = pattern.matcher(operations);
+
+        List<Problem> problems = new ArrayList<>();
+
+        while (operationsMatcher.find()) {
+            var operation = switch (operationsMatcher.group(0)) {
+                case "+" -> Operation.ADD;
+                case "*" -> Operation.MULTIPLY;
+                default -> throw new AocSolveException("Unexpected operation: " + operationsMatcher.group(0));
+            };
+
+            problems.add(new Problem(operation, new ArrayList<>()));
+        }
+
+        // Parse numbers
+        for (int i = 0; i < input.length - 1; i++) {
+            Matcher numbersMatcher = pattern.matcher(String.valueOf(input[i]));
+
+            for (Problem problem : problems) {
+                if (!numbersMatcher.find()) {
+                    throw new AocSolveException("Numbers not parsed correctly");
+                }
+
+                problem.numbers().add(Long.parseLong(numbersMatcher.group()));
+            }
+        }
+
+        return Long.toString(problems.stream().mapToLong(Problem::solve).sum());
     }
 
     @Override
     public String part2() throws AocSolveException {
-        return null;
+        List<Problem> problems = new ArrayList<>();
+        Problem current = null;
+
+        for (int x = 0; x < input[0].length; x++) {
+            // If all digits are empty (or we are at the end of the input), we have passed a problem.
+            int finalX = x;
+            if (IntStream.range(0, input.length).allMatch(y -> input[y][finalX] == ' ')) {
+                current = null;
+                continue;
+            }
+
+            // Read current operation and initialize problem
+            if (input[input.length - 1][x] == '+') {
+                current = new Problem(Operation.ADD, new ArrayList<>());
+                problems.add(current);
+            } else if (input[input.length - 1][x] == '*') {
+                current = new Problem(Operation.MULTIPLY, new ArrayList<>());
+                problems.add(current);
+            }
+
+            // Read number
+            StringBuilder builder = new StringBuilder();
+            for (int y = 0; y < input.length - 1; y++) {
+                builder.append(input[y][x]);
+            }
+
+            // Add the number to the current problem
+            assert current != null;
+            current.numbers().add(Long.parseLong(builder.toString().strip()));
+        }
+
+        return Long.toString(problems.stream().mapToLong(Problem::solve).sum());
+    }
+
+    enum Operation {
+        ADD, MULTIPLY
+    }
+
+    record Problem(Operation operation, List<Long> numbers) {
+        private long solve() {
+            return switch (operation) {
+                case ADD -> numbers.stream().reduce(0L, Long::sum);
+                case MULTIPLY -> numbers.stream().reduce(1L, (a, b) -> a * b);
+            };
+        }
     }
 }
